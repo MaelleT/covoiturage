8cee9a9c32701f1314dfaecc4af0960955f2994e
MaelleT <maelle.taurand@ac-nantes.fr>
2016-08-03 21:38:04 +0200

    Activation du modèle Première class model TypeOffre modifiable via interface admin

--- Covoiturage/settings.py
+++ Covoiturage/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',  # session framework 
     'django.contrib.messages', # messaging framework
     'django.contrib.staticfiles', #framework for managing static files
+    'covoit.apps.CovoitConfig'
 ]
 
 MIDDLEWARE_CLASSES = [
--- covoit/admin.py
+++ covoit/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from covoit.models import TypeOffre
 
 # Register your models here.
+admin.site.register(TypeOffre)
