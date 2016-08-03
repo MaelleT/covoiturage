b9cee04005dd777edbed91232f4083603344b1c8
MaelleT <maelle.taurand@ac-nantes.fr>
2016-08-03 23:20:06 +0200

    Index : affichage des offres avec un template

--- covoit/views.py
+++ covoit/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 from django.http.response import HttpResponse
 from covoit.models import OffrePermanente
+from django.template import loader
 
 
 def index(request):
+    
     offres_list = OffrePermanente.objects.order_by('lieu')
-    output = ' | '.join([str(o) for o in offres_list])
-    return HttpResponse(output)
+    
+    template = loader.get_template('covoit/index.html')
+    context={
+             'offres_list':offres_list
+             }    
+    return HttpResponse(template.render(context,request))
+    
