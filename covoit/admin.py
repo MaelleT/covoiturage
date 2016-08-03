bba6935f904fe28bc1d026c0bc7226be23a63739
MaelleT <maelle.taurand@ac-nantes.fr>
2016-08-03 22:16:49 +0200

    ajout des classes Model Jour et offrePermanentes

--- covoit/admin.py
+++ covoit/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from covoit.models import TypeOffre, Lieu
+from covoit.models import TypeOffre, Lieu, Jour, OffrePermanente
 
 # Register your models here.
 admin.site.register(TypeOffre)
 admin.site.register(Lieu)
+admin.site.register(Jour)
+admin.site.register(OffrePermanente)
--- covoit/models.py
+++ covoit/models.py
@@ -7,10 +7,33 @@ class TypeOffre(models.Model):
     def __str__(self):
         return self.libelle
     
+    def isDepart(self):
+        return (self.libelle == "Départ")
+    
 
 class Lieu(models.Model):
     libelle=models.CharField(max_length=50)
     
+    def __str__(self):
+        return self.libelle
+    
+class Jour(models.Model):
+    libelle=models.CharField(max_length=10)
     
     def __str__(self):
         return self.libelle
+    
+class OffrePermanente(models.Model):
+    lieu=models.ForeignKey(Lieu)
+    type=models.ForeignKey(TypeOffre)
+    jour=models.ForeignKey(Jour)
+    
+    def __str__(self):
+        res = "permanent à Xh le " + str(self.jour)
+    
+        if self.type.isDepart() :
+            res = res + " de " 
+        else :
+            res = res + " pour "
+        
+        return res + str(self.lieu)
