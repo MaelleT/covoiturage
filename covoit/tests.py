2b6bade841657b9f8e65ec2771a3dfb8fa3aba66
MaelleT <maelle.taurand@ac-nantes.fr>
2016-08-09 14:08:21 +0200

    Model test : mise à jour de la classe de test suite à modification du modèle

--- covoit/tests.py
+++ covoit/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from covoit.models import OffrePermanente, TypeOffre, Lieu, Jour
 from posix import unlink
+from django.contrib.auth.models import User
 
 # Create your tests here.
 class OffrePermanenteMethodTests(TestCase):
@@ -10,7 +11,8 @@ class OffrePermanenteMethodTests(TestCase):
         self.unLieu = Lieu(libelle='Nantes - Nantes Nord')
         self.unType=TypeOffre(libelle = 'Départ')
         self.unJour=Jour('lundi')
-        self.OffrePermanenteDep=OffrePermanente(lieu=self.unLieu,type=self.unType,jour=self.unJour)
+        self.unUser=User.objects.create_user('test', 'test@test.com', 'test1234')
+        self.OffrePermanenteDep=OffrePermanente(lieu=self.unLieu,type=self.unType,jour=self.unJour,auteur=self.unUser)
         
     def tearDown(self):
         TestCase.tearDown(self)
@@ -20,3 +22,6 @@ class OffrePermanenteMethodTests(TestCase):
         test_isDepart_offre doit retourner True pour les questions de type Départ
         """
         self.assertEqual(self.OffrePermanenteDep.isDepart(),True,"Erreur : Offre de type départ") 
+    
+
+
